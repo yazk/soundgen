899627ea85c54de1f75ba9910e84d55cae36a907
Yaz Khabiri <yazk@users.noreply.github.com>
2017-08-11 01:14:00 -0700

    clean up

--- soundgen/main.cpp
+++ soundgen/main.cpp
@@ -22,7 +22,7 @@ double linChirp(double time, double freqStart, double freqEnd, double sweepTime)
 	k = (freqEnd - freqStart) / sweepTime;
 	sound = sin(phaseStart + 2.0 * PI * (freqStart * time + k / 2.0 * time * time));
 
-	//chirp *= clamp(sign(sweepTime - time), 0.0, 1.0);
+	//sound *= clamp(sign(sweepTime - time), 0.0, 1.0);
 
 	return sound;
 }
@@ -36,7 +36,7 @@ double expChirp(double time, double freqStart, double freqEnd, double sweepTime)
 	k = pow(freqEnd / freqStart, 1.0 / sweepTime);
 	sound = sin(phaseStart + 2.0 * PI * freqStart * (pow(k, time) - 1.0) / log(k));
 
-	//chirp *= clamp(sign(sweepTime - time), 0.0, 1.0);
+	//sound *= clamp(sign(sweepTime - time), 0.0, 1.0);
 
 	return sound;
 }
@@ -52,12 +52,10 @@ double fade(double intervalPercent)
 	return exp(-pow(intervalPercent, 4.0) * 7.);
 }
 
-double linShepard(double time)
+double linShepard(double time, double f = 27.5, double sweepTime = 2.0)
 {
 	double sound = 0.0;
-	double sweepTime = 2.0;
-	const double f = 27.5;
-	double orgtime = time;
+
 	time = fmod(time, sweepTime);
 
 	double loopPercent = time / sweepTime;
@@ -71,79 +69,36 @@ double linShepard(double time)
 	sound += 0.125 * linChirp(time, f * 64, f * 128, sweepTime);
 	sound += 0.125 * linChirp(time, f * 128, f * 256, sweepTime);
 
-	return (sound);
+	return sound;
 }
 
 double expShepard(double time, double f1, double f2, double sweepTime)
 {
 	double sound = 0.0;
 
-	// old method, doubling frequency, time = time % sweepTime
-	//sound += 0.125 * chirp(orgtime, f * 1,   f * 2,   sweepTime);
-	//sound += 0.125 * chirp(orgtime, f * 2,   f * 4,   sweepTime);
-	//sound += 0.125 * chirp(orgtime, f * 4,   f * 8,   sweepTime);
-	//sound += 0.125 * chirp(orgtime, f * 8,   f * 16,  sweepTime);
-	//sound += 0.125 * chirp(orgtime, f * 16,  f * 32,  sweepTime);
-	//sound += 0.125 * chirp(orgtime, f * 32,  f * 64,  sweepTime);
-	//sound += 0.125 * chirp(orgtime, f * 64,  f * 128, sweepTime);
-	//sound += 0.125 * chirp(orgtime, f * 128, f * 256, sweepTime);
-	
 	time = fmod(time, sweepTime);
+
 	double intervalPercent = time / sweepTime;
+
+	// old method, doubling frequency, time = time % sweepTime
+	// f*1 -> f*2 + f*2 -> f*4, etc
 
 	// new method, double sweepTime, use same freq range
 	// better because there is no continuity issue (phase) between intervals
+	// fade top and bottom frequencies
 #define NUM_OCTAVES 8
-	sound += (1- fade(intervalPercent)) *chirp(time + sweepTime * 0., f1, f2, sweepTime);
-	sound += chirp(time + sweepTime * 1., f1, f2, sweepTime);
-	sound += chirp(time + sweepTime * 2., f1, f2, sweepTime);
-	sound += chirp(time + sweepTime * 3., f1, f2, sweepTime);
-	sound += chirp(time + sweepTime * 4., f1, f2, sweepTime);
-	sound += chirp(time + sweepTime * 5., f1, f2, sweepTime);
-	sound += chirp(time + sweepTime * 6., f1, f2, sweepTime);
-	sound += fade(intervalPercent) * chirp(time + sweepTime * 7., f1, f2, sweepTime);
+	sound += (1- fade(intervalPercent)) *	expChirp(time + sweepTime * 0., f1, f2, sweepTime);
+	sound +=								expChirp(time + sweepTime * 1., f1, f2, sweepTime);
+	sound +=								expChirp(time + sweepTime * 2., f1, f2, sweepTime);
+	sound +=								expChirp(time + sweepTime * 3., f1, f2, sweepTime);
+	sound +=								expChirp(time + sweepTime * 4., f1, f2, sweepTime);
+	sound +=								expChirp(time + sweepTime * 5., f1, f2, sweepTime);
+	sound +=								expChirp(time + sweepTime * 6., f1, f2, sweepTime);
+	sound += fade(intervalPercent) *		expChirp(time + sweepTime * 7., f1, f2, sweepTime);
 
 	sound *= 1. / NUM_OCTAVES;
 
 	return sound;
-}
-
-// https://en.wikipedia.org/wiki/File:DescenteInfinie.ogg
-#define Period 20.
-#define A0 0.7
-
-float a(float t)
-{
-	return A0 * (1. + cos(PI * t / Period)) / 4.;
-}
-
-float w(float t, float freq)
-{
-	return 2. * PI * freq * 1. / pow(2., t / Period);
-}
-
-float s(float t, float freq)
-{
-	return a(t) * sin(w(t, freq) * t) + a(t + Period) * sin(2. * w(t, freq) * t);
-}
-
-double expShepard_wikipedia(double time)
-{
-	double sound = 0.0;
-
-	sound += s(time, 440.);
-	sound += s(time, 523.25);
-
-	sound += s(time, 440.*2.);
-	sound += s(time, 523.25*2.);
-
-	sound += s(time, 440.*4.);
-	sound += s(time, 523.25*4.);
-
-	sound += s(time, 440.*8.);
-	sound += s(time, 523.25*8.);
-
-	return 0.25 * sound;
 }
 
 int main()
@@ -166,7 +121,20 @@ int main()
 
 	while (curTime < totalTime)
 	{
-		wav[curSample] = expShepard(curTime, 27.5, 55, 15.0);
+		wav[curSample] = 0;
+		wav[curSample] += expShepard(curTime, 27.5, 55, 15.0);
+		//wav[curSample] += expShepard(curTime, 29.14, 58.27, 15.0);
+		//wav[curSample] += expShepard(curTime, 30.87, 61.74, 15.0);
+		//wav[curSample] += expShepard(curTime, 32.70, 65.41, 15.0);
+		//wav[curSample] += expShepard(curTime, 34.65, 69.30, 15.0);
+		//wav[curSample] += expShepard(curTime, 36.71, 73.42, 15.0);
+		//wav[curSample] += expShepard(curTime, 38.89, 77.78, 15.0);
+		//wav[curSample] += expShepard(curTime, 41.20, 82.41, 15.0);
+		//wav[curSample] += expShepard(curTime, 43.65, 87.31, 15.0);
+		//wav[curSample] += expShepard(curTime, 46.25, 92.50, 15.0);
+		//wav[curSample] += expShepard(curTime, 49.00, 98.00, 15.0);
+		//wav[curSample] += expShepard(curTime, 51.91, 103.83, 15.0);
+		//wav[curSample] *= 1./12.;
 		//wav[curSample] = expShepard_wikipedia(curTime);
 
 		curTime = ++curSample / SAMPLE_RATE;
